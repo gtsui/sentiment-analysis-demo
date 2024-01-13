266f67d493458412d367845aed1703e99b4a810e
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2024-01-13 12:17:46 +0800

    add link

--- src/components/app/tweet/TweetContainer.tsx
+++ src/components/app/tweet/TweetContainer.tsx
@@ -1,6 +1,8 @@
-import { toDateStr, toDateTimeStr } from "@/src/lib/dateUtils";
+import Image from "next/image";
+import { toDateTimeStr } from "@/src/lib/dateUtils";
 import { ITweet } from "@/src/types/types";
 import { CardDark1, CardDark2 } from "../../common/card/Cards";
+import NewWindow from "@/src/assets/icons/ui/new_window.svg";
 
 type Props = {
   tweet: ITweet;
@@ -10,9 +12,21 @@ const TweetContainer = ({ tweet }: Props) => {
   return (
     <>
       <CardDark1 className="flex flex-col p-6 gap-2">
-        <div className="flex flex-row gap-2">
+        <div className="flex flex-row gap-2 items-center">
           <h4 className="text-primary-500">{tweet.username}</h4>
           <h4 className="text-contrast-low">{toDateTimeStr(tweet.ts)}</h4>
+          <a
+            href={`https://www.twitter.com${tweet.url}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <NewWindow
+              className="text-contrast-medium"
+              alt=""
+              height={12}
+              width={12}
+            />
+          </a>
         </div>
         <CardDark2 className="flex p-6">
           <h4 className="text-contrast-high">{tweet.content}</h4>
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,7 @@
 export type ITweet = {
   ts: number;
   username: string;
+  url: string;
   author: string;
   content: string;
   isRetweet: boolean;
