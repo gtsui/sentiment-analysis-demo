1881b0ce6a05539c90587eaecb70685fe7b51ca2
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2024-01-14 17:17:08 +0800

    sorting

--- app/api/get-users/route.ts
+++ app/api/get-users/route.ts
@@ -5,7 +5,9 @@ import UserModel from "@/src/db/models/userSchema";
 export const GET = async () => {
   try {
     await connectDB();
-    let users = await UserModel.find({}).sort({ username: 1 });
+    let users = await UserModel.find({})
+      .collation({ locale: "en", strength: 2 })
+      .sort({ username: 1 });
     return NextResponse.json(users);
   } catch (e: any) {
     return NextResponse.json([]);
--- src/db/connect.ts
+++ src/db/connect.ts
@@ -4,7 +4,6 @@ let dbConnected = false;
 
 export const connectDB = async () => {
   if (dbConnected) {
-    console.log("calling db...");
     return;
   }
 
